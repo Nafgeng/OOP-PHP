c0f81360270b2b4e51867d50b23bfd0580cd2eed
Nafgeng <nafgeng@gmail.com>
2023-02-28 11:32:58 +0700

    ExceptionHandling

---
+++ ExceptionHandling.php
@@ -0,0 +1,16 @@
+<<?php
+class Errorable
+{
+public static function calculate(int $number)
+{
+return ($number % 0);
+}
+}
+try {
+Errorable::calculate(7);
+} catch (DivisionByZeroError $e) {
+echo $e->getMessage();
+echo PHP_EOL;
+}
+echo 'Ini tetap dieksekusi';
+echo PHP_EOL;
