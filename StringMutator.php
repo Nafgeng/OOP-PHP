72946de6decb4af5aafbb90be6e20a98f29b88cb
Nafgeng <nafgeng@gmail.com>
2023-02-28 11:33:08 +0700

    StringMutator

--- StringMutator.php
+++ StringMutator.php
@@ -1,21 +1,38 @@
 <?php
-namespace Mutator;
-class StringMutator
+// namespace Mutator;
+// class StringMutator
+// {
+// public static function bold(string $word)
+// {
+// return sprintf('<b>%s</b>', $word);
+// }
+// public static function underline(string $word)
+// {
+// return sprintf('<u>%s</u>', $word);
+// }
+// public static function italic(string $word)
+// {
+// return sprintf('<i>%s</i>', $word);
+// }
+// public static function strike(string $word)
+// {
+// return sprintf('<strike>%s</strike>', $word);
+// }
+// }
+
+// Trait StringMutator
+trait StringMutator
 {
-public static function bold(string $word)
+public function bold(string $word): string
 {
 return sprintf('<b>%s</b>', $word);
 }
-public static function underline(string $word)
-{
-return sprintf('<u>%s</u>', $word);
-}
-public static function italic(string $word)
+public function italic(string $word): string
 {
 return sprintf('<i>%s</i>', $word);
 }
-public static function strike(string $word)
+public function boldItalic(string $word): string
 {
-return sprintf('<strike>%s</strike>', $word);
+return $this->italic($this->bold($word));
 }
 }
